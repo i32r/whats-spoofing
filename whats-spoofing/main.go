f2664d1af0f66cab522eed72064be2c1abbb6a4a
i32r <108593110+i32r@users.noreply.github.com>
2024-06-29 00:07:20 +0400

    Add files via upload

---
+++ whats-spoofing/main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"flag"
+	"os"
+	"os/signal"
+	"strings"
+	"sync/atomic"
+	"syscall"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
+	"google.golang.org/protobuf/proto"
+
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/appstate"
+	waBinary "go.mau.fi/whatsmeow/binary"
+	"go.mau.fi/whatsmeow/store"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
+)
+
+var cli *whatsmeow.Client
+var log waLog.Logger
+
+var logLevel = "INFO"
+var debugLogs = flag.Bool("debug", false, "Enable debug logs?")
+var dbDialect = flag.String("db-dialect", "sqlite3", "Database dialect (sqlite3 or postgres)")
+var dbAddress = flag.String("db-address", "file:db/whatsbot.db?_foreign_keys=on", "Database address")
+var requestFullSync = flag.Bool("request-full-sync", false, "Request full (1 year) history sync when logging in?")
+var mediaPath = flag.String("media-path", "media", "Path to store media files in")
+var historyPath = flag.String("history-path", "history", "Path to store history files in")
+var pairRejectChan = make(chan bool, 1)
+
+func main() {
+
+	waBinary.IndentXML = true
+	flag.Parse()
+
+	if *debugLogs {
+		logLevel = "DEBUG"
+	}
+	if *requestFullSync {
+		store.DeviceProps.RequireFullSync = proto.Bool(true)
+	}
+	log = waLog.Stdout("Main", logLevel, true)
+
+	dbLog := waLog.Stdout("Database", logLevel, true)
+	storeContainer, err := sqlstore.New(*dbDialect, *dbAddress, dbLog)
+	if err != nil {
+		log.Errorf("Failed to connect to database: %v", err)
+		return
+	}
+	device, err := storeContainer.GetFirstDevice()
+	if err != nil {
+		log.Errorf("Failed to get device: %v", err)
+		return
+	}
+
+	cli = whatsmeow.NewClient(device, waLog.Stdout("Client", logLevel, true))
+	var isWaitingForPair atomic.Bool
+	cli.PrePairCallback = func(jid types.JID, platform, businessName string) bool {
+		isWaitingForPair.Store(true)
+		defer isWaitingForPair.Store(false)
+		log.Infof("Pairing %s (platform: %q, business name: %q). Type r within 3 seconds to reject pair", jid, platform, businessName)
+		select {
+		case reject := <-pairRejectChan:
+			if reject {
+				log.Infof("Rejecting pair")
+				return false
+			}
+		case <-time.After(3 * time.Second):
+		}
+		log.Infof("Accepting pair")
+		return true
+	}
+
+	ch, err := cli.GetQRChannel(context.Background())
+	if err != nil {
+		// This error means that we're already logged in, so ignore it.
+		if !errors.Is(err, whatsmeow.ErrQRStoreContainsID) {
+			log.Errorf("Failed to get QR channel: %v", err)
+		}
+	} else {
+		go func() {
+			for evt := range ch {
+				if evt.Event == "code" {
+					qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+				} else {
+					log.Infof("QR channel result: %s", evt.Event)
+				}
+			}
+		}()
+	}
+
+	cli.AddEventHandler(handler)
+	err = cli.Connect()
+	if err != nil {
+		log.Errorf("Failed to connect: %v", err)
+		return
+	}
+
+	c := make(chan os.Signal, 1)
+	input := make(chan string)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		defer close(input)
+		scan := bufio.NewScanner(os.Stdin)
+		for scan.Scan() {
+			line := strings.TrimSpace(scan.Text())
+			if len(line) > 0 {
+				input <- line
+			}
+		}
+	}()
+	for {
+		select {
+		case <-c:
+			log.Infof("Interrupt received, exiting")
+			cli.Disconnect()
+			return
+		case cmd := <-input:
+			if len(cmd) == 0 {
+				log.Infof("Stdin closed, exiting")
+				cli.Disconnect()
+				return
+			}
+			if isWaitingForPair.Load() {
+				if cmd == "r" {
+					pairRejectChan <- true
+				} else if cmd == "a" {
+					pairRejectChan <- false
+				}
+				continue
+			}
+			args := strings.Fields(cmd)
+			cmd = args[0]
+			args = args[1:]
+			go handleCmd(strings.ToLower(cmd), args)
+		}
+	}
+}
+
+func parseJID(arg string) (types.JID, bool) {
+	if arg[0] == '+' {
+		arg = arg[1:]
+	}
+	if !strings.ContainsRune(arg, '@') {
+		return types.NewJID(arg, types.DefaultUserServer), true
+	} else {
+		recipient, err := types.ParseJID(arg)
+		if err != nil {
+			log.Errorf("Invalid JID %s: %v", arg, err)
+			return recipient, false
+		} else if recipient.User == "" {
+			log.Errorf("Invalid JID %s: no server specified", arg)
+			return recipient, false
+		}
+		return recipient, true
+	}
+}
+
+var historySyncID int32
+var startupTime = time.Now().Unix()
+
+func handler(rawEvt interface{}) {
+	switch evt := rawEvt.(type) {
+	case *events.AppStateSyncComplete:
+		if len(cli.Store.PushName) > 0 && evt.Name == appstate.WAPatchCriticalBlock {
+			err := cli.SendPresence(types.PresenceAvailable)
+			log.Errorf("AppStateSyncComplete %s: %v", rawEvt, err)
+		}
+		return
+	case *events.Connected, *events.PushNameSetting:
+		if len(cli.Store.PushName) == 0 {
+			return
+		}
+		err := cli.SendPresence(types.PresenceAvailable)
+		log.Errorf("Connected %s: %v", rawEvt, err)
+		return
+	case *events.StreamReplaced:
+		os.Exit(0)
+	}
+}
+
+func handleCmd(cmd string, args []string) {
+	switch cmd {
+	case "getgroup":
+		cmdGetGroup(args)
+	case "listgroups":
+		cmdListGroups(args)
+	case "send-spoofed-reply":
+		cmdSendSpoofedReply(args)
+	case "send-spoofed-img-reply":
+		cmdSendSpoofedImgReply(args)
+	case "send-spoofed-demo":
+		cmdSendSpoofedDemo(args)
+	case "send-spoofed-demo-img":
+		cmdSendSpoofedDemoImg(args)
+	}
+}
